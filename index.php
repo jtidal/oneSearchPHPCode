ce0f53f3669c64dbae0cea77794a361a394a793f
Junior Tidal <jtidal@citytech.cuny.edu>
2014-11-19 15:01:39 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+
+<p><b>New! Try <a href="http://onesearch.cuny.edu/primo_library/libweb/action/search.do?vid=ny">OneSearch</a> to find books, articles, ebooks, and media.</b></p>
+
+<p><a href="http://onesearch.cuny.edu/primo_library/libweb/action/search.do?vid=ny"><img src="http://library.citytech.cuny.edu/uploads/onesearch_logo.png" /></a></p>
+
+<form action="http://library.citytech.cuny.edu/php/oneSearch.php" enctype="application/x-www-form-urlencoded; charset=utf-8" method="post" name="searchForm" role="search">
+
+	<select name="selectStyle" style="float:left; margin: 0px 5px 5px 0px">
+	<option>Everything</option>
+	<option>Articles</option>
+	<option>Books (Print + eBooks)</option>
+	<option>Books (Print)</option>
+	<option>eBooks</option>
+	<option>Media</option></select> 
+
+	<input name="institution" type="hidden" value="NY" /> <input name="vid" type="hidden" value="ny" /> <input name="group" type="hidden" value="GUEST" /> <input name="onCampus" type="hidden" value="true" />
+
+	<input name="search_scope" type="hidden" value="everything" /> <input id="primoQuery" name="query" type="hidden" /> 
+
+	<input id="primoQueryTemp" name="queryTemp" size="35" type="text" value="" /> 
+
+	<input alt="Search" class="button" id="submit" title="Search books, articles &amp; more" type="submit" value="Search" />&nbsp;
+</form>
+
+<p><a href="http://citytech.ezproxy.cuny.edu:2048/login?url=http://libsearch.cuny.edu/F?func=BOR-INFO">My Account | </a><a href="http://library.citytech.cuny.edu/research/otherLibraries/index.php">Other Libraries</a> | <a href="http://library.citytech.cuny.edu/services/faculty/ill/index.php">Interlibrary Loan (For Faculty)</a>&nbsp;| <a href="http://library.citytech.cuny.edu/comments.php">Comments</a></p>
+
+</html>
+</body>
