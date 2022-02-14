4e218605a3fa8a83ba74c6f8f490475d4368733c
Junior Tidal <jtidal@citytech.cuny.edu>
2022-02-14 17:50:20 -0500

    Update oneSearch.php

--- oneSearch.php
+++ oneSearch.php
@@ -1,43 +1,50 @@
 <?php
- 
+
  $searchSelect = $_POST["selectStyle"];
  $theQuery = $_POST["queryTemp"];
- 
- //echo $searchSelect;
- //echo '<br>'.$theQuery;
+ $radioSelection = $_POST["radioType"];
 
 
- $redirect = 'http://onesearch.cuny.edu/primo_library/libweb/action/dlSearch.do?institution=NY&vid=ny&group=GUEST&onCampus=true';
+ $redirect = 'https://cuny-ny.primo.exlibrisgroup.com/discovery/search?vid=01CUNY_NY:CUNY_NY&search_scope=IZ_CI_AW';
 
 switch ($searchSelect){
-	case "Everything":
-		$redirect = $redirect.'&search_scope=everything&query=any%2Ccontains%2C'.$theQuery.'&queryTemp='.$theQuery;
-		header('Location:'.$redirect);
-		break;	
-		
-	case "Articles":
-		$redirect = $redirect.'&search_scope=everything&fctN=facet_rtype&fctV=articles&rfnGrp=1&rfnGrpCounter=1&highlight=true&displayField=title&query=any%2Ccontains%2C'.$theQuery.'&queryTemp='.$theQuery;
-		header('Location:'.$redirect);
-		break;
+        case "Everything":
 
-	case "Books (Print + eBooks)":
-		$redirect = $redirect.'&search_scope=everything&fctN=facet_rtype&fctV=books&rfnGrp=1&rfnGrpCounter=1&highlight=true&displayField=title&query=any%2Ccontains%2C'.$theQuery.'&queryTemp='.$theQuery;
-		header('Location:'.$redirect);
-		break;
+                $redirect = $redirect.'&tab=Everything&mode=basic&highlight=true&displayMode=full&bulkSize=10&dum=true&displayField=all&pcAvailabilityMode=false&query='.$radioSelection.',contains,'.$theQuery;
+                header('Location:'.$redirect);
+                break;
 
-	case "Books (Print)": 
-			$redirect = $redirect.'&search_scope=everything&fctN=facet_tlevel&fctV=available&rfnGrp=show_only&fctN=facet_rtype&fctV=books&rfnGrp=1&rfnGrpCounter=1&highlight=true&displayField=title&query=any%2Ccontains%2C'.$theQuery.'&queryTemp='.$theQuery;
-		header('Location:'.$redirect);
-		break;
+        case "Articles":
 
-	case "eBooks":
-			$redirect = $redirect.'&search_scope=everything&fctN=facet_tlevel&fctV=online_resources&rfnGrp=show_only&fctN=facet_rtype&fctV=books&rfnGrp=1&rfnGrpCounter=1&highlight=true&displayField=title&query=any%2Ccontains%2C'.$theQuery.'&queryTemp='.$theQuery;
-		header('Location:'.$redirect);
-		break;
+                $redirect = $redirect.'&tab=Everything&mode=basic&displayMode=full&bulkSize=10&dum=true&displayField=all&facet=rtype,include,articles&query='.$radioSelection.',contains,'.$theQuery;
+                header('Location:'.$redirect);
+                break;
 
-	case "Media":
-			$redirect = $redirect.'&search_scope=everything&fctN=facet_rtype&fctV=media&rfnGrp=1&rfnGrpCounter=1&highlight=true&displayField=title&query=any%2Ccontains%2C'.$theQuery.'&queryTemp='.$theQuery;
-		header('Location:'.$redirect);
-		break;
+        case "Books (Print + eBooks)":
 
-}?>
+                $redirect = $redirect.'&tab=Everything&mode=basic&highlight=true&displayMode=full&bulkSize=10&dum=true&displayField=all&facet=rtype,include,books&pcAvailabiltyMode=false&query='.$radioSelection.',contains,'.$theQuery;
+                header('Location:'.$redirect);
+                break;
+
+        case "Books (Print)":
+
+                $redirect = $redirect.'&mode=basic&highlight=true&displayMode=full&bulkSize=10&dum=true&displayField=all&facet=rtype,include,books&face=tlevel,include,available_p&pcAvailabilityMode=false&query='.$radioSelection.',contains,'.$theQuery;
+		header('Location:'.$redirect);
+                break;
+
+        case "eBooks":
+
+                $redirect = $redirect.'&mode=basic&highlight=true&displayMode=full&bulkSize=10&dum=true&displayField=all&facet=rtype,include,books&facet=tlevel,include,online_resources&pcAvailabilityMode=false&query='.$radioSelection.',contains,'.$theQuery;
+                header('Location:'.$redirect);
+                break;
+
+        case "Media":
+
+                $redirect = $redirect.'&mode=basic&highlight=true&displayMode=full&bulkSize=10&dum=true&displayField=all&facet=rtype,include,videos&pcAvailabilityMode&query='.$radioSelection.',contains,'.$theQuery;
+                header('Location:'.$redirect);
+                break;
+
+}
+?>
+
+
